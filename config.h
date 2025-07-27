28243b7518efc7501ed0203e37d3e50be563d9f0
v3natio <72844565+v3natio@users.noreply.github.com>
2025-07-27 18:07:52 +0200

    added auto-rotate keybind, fixed virtual keyboard

--- config.h
+++ config.h
@@ -136,6 +136,7 @@ static const Key keys[] = {
 	{ MODKEY, XK_d, spawn, SHCMD("dmenu_recency") },
 	{ MODKEY, XK_Return, spawn, {.v = termcmd } },
   { MODKEY|ShiftMask, XK_Return, spawn, SHCMD("samedir") },
+  { 0, XF86XK_Launch1, spawn, SHCMD("getrotate") },
   { MODKEY, XK_q, killclient, {0} },
   { MODKEY|ShiftMask, XK_e, quit, {0} },
 
@@ -162,7 +163,9 @@ static const Key keys[] = {
 
   // brightness
   { 0, XF86XK_MonBrightnessUp, spawn, SHCMD("brightnessctl --device='intel_backlight' --quiet set +5%") },
+  { MODKEY, XK_F4, spawn, SHCMD("brightnessctl --device='intel_backlight' --quiet set +5%") },
 	{ 0, XF86XK_MonBrightnessDown, spawn, SHCMD("brightnessctl --device='intel_backlight' --quiet set 5%-") },
+  { MODKEY, XK_F5, spawn, SHCMD("brightnessctl --device='intel_backlight' --quiet set 5%-") },
   { 0, XF86XK_Launch3, spawn, SHCMD("xsct -t") },
 
   // multimedia
